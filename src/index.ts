aad1ee3ee8f5f33e5abbdaa59027953d989e1ee8
Sam Jarvis <jarvisam@gmail.com>
2020-06-06 22:52:28 +0100

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,26 @@ import express from 'express';
 import Mongo from './mongo';
 import {getFundById} from './vanguard';
 
+import {format, transports} from 'winston';
+import expressWinston from 'express-winston';
+
 const app = express();
 const mClient = new Mongo();
+
+const expressLogger = expressWinston.logger({
+  transports: [
+    new transports.Console(),
+    new transports.File({filename: 'error.log', level: 'error'}),
+    new transports.File({filename: 'combined.log'}),
+  ],
+  format: format.combine(
+      format.colorize(),
+      format.json(),
+  ),
+  colorize: true,
+});
+
+app.use(expressLogger);
 
 const round2dp = (number:number): number => {
   return Math.round((number+Number.EPSILON)*100)/100;
@@ -33,7 +51,6 @@ app.get('/currentWorth', async (req, res)=>{
       value,
     };
   }));
-  console.log(fundValues);
   const totalWorth = round2dp(fundValues
       .reduce((worth, fund)=> worth+fund.value, 0));
   res.json({
