daee353d3dbe93fdd58fa3ea3e11a608001db239
Sam Jarvis <jarvisam@gmail.com>
2020-06-06 21:21:17 +0100

    use the REST Sensor pattern in HA

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import express from 'express';
 import Mongo from './mongo';
 import {getFundById} from './vanguard';
-import axios from 'axios';
-import {readFileSync} from 'fs';
 
 const app = express();
 const mClient = new Mongo();
-
-const HASS_API_KEY = readFileSync('./HASS_API_KEY', 'utf-8');
-console.log(HASS_API_KEY);
 
 const round2dp = (number:number): number => {
   return Math.round((number+Number.EPSILON)*100)/100;
@@ -41,19 +36,6 @@ app.get('/currentWorth', async (req, res)=>{
   console.log(fundValues);
   const totalWorth = round2dp(fundValues
       .reduce((worth, fund)=> worth+fund.value, 0));
-  axios.post('http://192.168.8.113:8123/api/states/sensor.vanguard_api', {
-    state: totalWorth,
-    attributes: {
-      unit_of_measurement: '£',
-      friendly_name: 'Vanguard Value',
-      fundValues,
-    },
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${HASS_API_KEY}`,
-    },
-  });
   res.json({
     totalWorth,
     breakdown: fundValues,
